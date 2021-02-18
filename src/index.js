9cc09988301afdecc60cb763496103bf3e0f8d47
liuxingyu521 <xuj_power@qq.com>
2021-02-18 16:37:23 +0800

    fix: 兼容请求图片失败的情况

--- src/index.js
+++ src/index.js
@@ -23,11 +23,12 @@ module.exports = class TinyimgWebpackPlugin {
 			const imgs = Object.keys(compilation.assets).filter(v => IMG_REGEXP.test(v));
 			if (!imgs.length) return Promise.resolve();
 			const promises = imgs.map(v => this.compressImg(compilation.assets, v));
-			const spinner = Ora("Image is compressing......").start();
+			const spinner = Ora("Images is compressing......").start();
 			return new Promise(resolve => {
         Promise.all(promises).then(res => {
-          spinner.stop();
-          logged && res.forEach(v => console.log(v));
+					spinner.stopAndPersist({text: 'Images compression done', symbol: '✅'});
+					// 输出错误，如果有的话
+					res.filter(info => logged || info.status === 'fail').forEach(v => console.log(v.msg));
           resolve()
         })
       })
@@ -37,16 +38,22 @@ module.exports = class TinyimgWebpackPlugin {
 		try {
 			const file = assets[path].source();
 			const obj = await this.uploadImg(file);
+
+			if (obj.isError) {
+				return Promise.resolve({msg: `${Figures.cross} [${Chalk.yellowBright(path)}] compression failed. ${obj.msg}`, status: 'fail'});
+			}		
+
 			const data = await this.downloadImg(obj.output.url);
+			// 写回到原来资源中
 			assets[path] = new RawSource(Buffer.alloc(data.length, data, 'binary'))
 			const oldSize = Chalk.redBright(ByteSize(obj.input.size));
 			const newSize = Chalk.greenBright(ByteSize(obj.output.size));
 			const ratio = Chalk.blueBright(RoundNum(1 - obj.output.ratio, 2, true));
 			const msg = `${Figures.tick} Compressed [${Chalk.yellowBright(path)}] completed: Old Size ${oldSize}, New Size ${newSize}, Optimization Ratio ${ratio}`;
-			return Promise.resolve(msg);
+			return Promise.resolve({msg, status: 'success'});
 		} catch (err) {
 			const msg = `${Figures.cross} Compressed [${Chalk.yellowBright(path)}] failed: ${Chalk.redBright(err)}`;
-			return Promise.resolve(msg);
+			return Promise.resolve({msg, status: 'fail'});
 		}
 	}
 	downloadImg(url) {
@@ -66,8 +73,12 @@ module.exports = class TinyimgWebpackPlugin {
 		const opts = RandomHeader();
 		return new Promise((resolve, reject) => {
 			const req = Https.request(opts, res => res.on("data", data => {
-				const obj = JSON.parse(data.toString());
-				obj.error ? reject(obj.message) : resolve(obj);
+				try {
+					const obj = JSON.parse(data.toString());
+					obj.error ? reject({isError: true, msg: obj.message}) : resolve(obj);
+				} catch (error) {
+					reject({isError: true, msg: error.message || '图片解析失败'})
+				}
 			}));
 			req.write(file, "binary");
 			req.on("error", e => reject(e));
